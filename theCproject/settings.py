5180e259dd7ec8143f1e5a91c2279dce83dd6936
Conan C. Albrecht <ca@byu.edu>
2014-08-26 13:08:58 -0600

    new db username

--- theCproject/settings.py
+++ theCproject/settings.py
@@ -63,8 +63,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'thecproject',
-        'USER': 'postgres',
-        'PASSWORD': 'Shikamaru1',
+        'USER': 'thecproject',
+        'PASSWORD': 'thecproject',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
