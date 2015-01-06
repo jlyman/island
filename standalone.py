f8976b5296297c0d1e5a5f3ff2f407d6c2b5d7b1
Conan C. Albrecht <ca@byu.edu>
2015-01-06 10:57:52 -0700

    change to standalone

--- standalone.py
+++ standalone.py
@@ -14,30 +14,4 @@ django.setup()
 from lib.filters import *
 from homepage import models as hmod
 from forum import models as fmod
-import random, datetime
 
-# main code
-user = hmod.SiteUser.objects.get(email='ca@byu.edu')
-topic = fmod.Topic.objects.get(pk=1)
-
-# clear out the tables for debugging - careful!
-print('Clearing old data.')
-fmod.Comment.objects.all().delete()
-fmod.Thread.objects.all().delete()
-
-now = datetime.datetime.now() - datetime.timedelta(days=5)
-
-# add new threads and comments
-print('Adding new threads and comments.')
-for thread_i in range(1, 50):
-  thread = fmod.Thread(user=user, topic=topic, title='Thread %s' % thread_i)
-  thread.save()
-  thread.created = now
-  thread.save()
-  for comment_i in range(1, random.randint(2, 20)):
-    comment = fmod.Comment(user=user, thread=thread, comment='Comment %s. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' % comment_i)
-    comment.save()
-    comment.created = now
-    comment.save()
-    
-    now += datetime.timedelta(minutes=random.randint(1, 10))
